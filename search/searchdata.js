e0833dbe616820b84ecfea01e2fa55f66f0c740a
Anshul Garg <32338571+pulse28@users.noreply.github.com>
2019-04-03 14:59:49 +0530

    Put this inside HTML folder

---
+++ search/searchdata.js
@@ -0,0 +1,24 @@
+var indexSectionsWithContent =
+{
+  0: "defghilmnopqrs",
+  1: "eflpqs",
+  2: "dfghilmnoprs",
+  3: "g"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "functions",
+  3: "variables"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Classes",
+  2: "Functions",
+  3: "Variables"
+};
+
